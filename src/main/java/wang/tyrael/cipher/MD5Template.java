d82346ca1d622b4af956c3f3949dec92235a0ce7
wangchao <wangchao@shouqianba.com>
2018-06-27 11:17:41 +0800

    1

---
+++ src/main/java/wang/tyrael/cipher/MD5Template.java
@@ -0,0 +1,9 @@
+package wang.tyrael.cipher;
+
+/**
+ * @Auther: wangchao
+ * @Date: 2018/6/27 10:34
+ * @Description:
+ */
+public class MD5Template {
+}
---
+++ src/main/java/wang/tyrael/cipher/MessageDigestTemplate.java
@@ -0,0 +1,9 @@
+package wang.tyrael.cipher;
+
+/**
+ * @Auther: wangchao
+ * @Date: 2018/6/27 10:27
+ * @Description:
+ */
+public class MessageDigestTemplate {
+}
